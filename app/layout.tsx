8dfa9993929fe9977f0ebd291ceeabd8e52c2516
Malwande-Dzanibe <malwandedza@gmail.com>
2024-02-24 18:19:33 +0200

    2024-02-24

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7333158740682287"
+             crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>
         <NavBar/>
         {children}
