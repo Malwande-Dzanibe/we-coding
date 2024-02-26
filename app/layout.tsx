360ea8c29dee413799eaff7e06551a9ac6266c93
Malwande-Dzanibe <malwandedza@gmail.com>
2024-02-26 16:19:11 +0200

    2024-02-26

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NavBar/>
         {children}
+        <Analytics/>
         <Footer/>
       </body>
     </html>
